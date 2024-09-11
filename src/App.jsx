45160ca48ad70fd15055426ebbac50ae97e6644b
Vikash kumar <vikashkumargauravdss@gmail.com>
2024-09-11 09:31:45 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import { Routes,Route } from 'react-router-dom'
+import Signup from './Pages/Signup';
+import Login from './Pages/Login';
+import Dashboard from './Pages/Dashboard';
+import Home from './Pages/Home'
+
+const App = () => {
+  return (
+    <>
+    <Navbar />
+
+    <Routes>
+      <Route path='/' element={<Home />} />;
+      <Route path='/dashboard' element={<Dashboard />} />;
+      <Route path='/login' element={<Login />} />;
+      <Route path='/signup' element={<Signup />} />
+
+
+    </Routes>
+    </>
+  )
+}
+
+export default App
